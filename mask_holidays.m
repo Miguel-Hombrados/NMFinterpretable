2acee4d7532191697b668061d61439b8917eca8c
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-01-04 22:17:00 -0700

    Create mask_holidays.m

---
+++ mask_holidays.m
@@ -0,0 +1,11 @@
+function [Mask] = mask_holidays(index_holidays)
+
+N = length(index_holidays);
+H = max(index_holidays);
+Mask = zeros(H,N);
+
+for hd=1:H
+    Mask(hd,index_yearday==hd) = 1;
+end
+
+end
