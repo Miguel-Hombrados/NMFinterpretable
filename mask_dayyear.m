eedce12dab4f57eba7b25260c27e780f425ae776
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-02-26 21:01:57 -0700

    Update mask_dayyear.m
    Change it to 365 days so the 366th day is considered 365th one.

--- mask_dayyear.m
+++ mask_dayyear.m
@@ -1,10 +1,24 @@
 function [Mask] = mask_dayyear(index_yearday)
 
+% N = length(index_yearday);
+% Mask = zeros(366,N);
+% 
+% for doy=1:366
+%     Mask(doy,index_yearday==doy) = 1;
+% end
+
 N = length(index_yearday);
-Mask = zeros(366,N);
+Mask = zeros(365,N);
 
 for doy=1:366
+    if doy ~=366
     Mask(doy,index_yearday==doy) = 1;
+    end
+    if doy ==355
+    Mask(365,index_yearday==doy)  = 1 ; 
+    end
 end
+% This is done due to the lack of leap days. The day 366 is considered the
+% 355th day.
 
 end
