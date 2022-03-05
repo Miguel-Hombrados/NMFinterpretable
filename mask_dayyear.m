d5da7f6a68e040183c006148e41da256b887f11d
Miguel A Hombrados <mangelhombradosherre@unm.edu>
2022-03-04 17:51:46 -0700

    Update mask_dayyear.m

--- mask_dayyear.m
+++ mask_dayyear.m
@@ -1,4 +1,4 @@
-function [Mask] = mask_dayyear(index_yearday)
+function [Mask,template_doy] = mask_dayyear(index_yearday)
 
 % N = length(index_yearday);
 % Mask = zeros(366,N);
@@ -9,14 +9,17 @@ function [Mask] = mask_dayyear(index_yearday)
 
 N = length(index_yearday);
 Mask = zeros(365,N);
-
+template_doy = {size(Mask,1),1};
 for doy=1:366
     if doy ~=366
     Mask(doy,index_yearday==doy) = 1;
+    template_doy{doy} = ['day' num2str(doy)];
     end
-    if doy ==355
+    if doy ==366
     Mask(365,index_yearday==doy)  = 1 ; 
+    template_doy{doy} = ['day' num2str(365)];
     end
+
 end
 % This is done due to the lack of leap days. The day 366 is considered the
 % 355th day.
